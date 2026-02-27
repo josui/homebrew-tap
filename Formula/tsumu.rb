d67a62f1bea8f06e6ba0d99bbc2cca6cbebca032
Bing Yang <josui.yang@gmail.com>
2026-02-27 23:04:22 +0900

    tsumu v0.0.7

--- Formula/tsumu.rb
+++ Formula/tsumu.rb
@@ -1,8 +1,8 @@
 class Tsumu < Formula
   desc "Local-first CLI bookmark manager"
   homepage "https://github.com/josui/tsumu-cli"
-  url "https://github.com/josui/tsumu-cli/archive/refs/tags/v0.0.6.tar.gz"
-  sha256 "57d1c700a5ae5ec70d0b735afa5cfd1d33f23e510813c80f378cff3bfe5f4bb1"
+  url "https://github.com/josui/tsumu-cli/archive/refs/tags/v0.0.7.tar.gz"
+  sha256 "2bb591d23413a7016589b68eb1450c326cda0dc702cb79bd60a61eabab035c6a"
   license "MIT"
 
   depends_on "go" => :build
